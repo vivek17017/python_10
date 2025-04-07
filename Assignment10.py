a9483530126e646c713b25fa31365691e7bab550
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-07 13:29:15 +0530

    Add files via upload

---
+++ Assignment10.py
@@ -0,0 +1,25 @@
+#Q1
+x=2534
+x=x//10
+print(x)
+
+#Q2
+x=2089
+x=x%10
+print(x)
+
+#Q3
+x=5
+y=8
+x=x+y
+y=x-y
+x=x-y
+print(x,y)
+#Q4
+x=int(input("Enter three digit number: "))
+x=x//100
+print(x)
+#Q5
+x=int(input("Enter three digit number: "))
+x=(x%100)//10
+print(x)
